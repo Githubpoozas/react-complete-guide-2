334e3b0d00ad1b29d1e717886cda8e5727f4847a
Githubpoozas <po.oz.as@hotmail.com>
2020-08-04 07:48:51 +0700

    first push

---
+++ src/Person/Person.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const person = props => {
+  return (
+    <div>
+      <p onClick={props.click}>
+        I'm {props.name || 'John'} and I am {props.age || 18} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  )
+}
+
+export default person
